cd20da61e64359d80ac451077cd4e22ebce72d7c
pyarena <Arman170616>
2021-04-13 10:40:25 +0600

    project upload

---
+++ Aambagan_django/product/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Category, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        # fileds = "__all__"
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        )
